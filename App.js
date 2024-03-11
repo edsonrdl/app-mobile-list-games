9d9771054c9cc258c9834c687923da50613e306c
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2024-03-11 00:36:55 -0300

    feat: Iniciado projeto lista de filmes

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
+import AppStyle from './AppStyle'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={AppStyle.container}>
+      <Text style={AppStyle.textStyle}>Olá</Text>
+      <StatusBar style="auto"/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+;
---
+++ AppStyle.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const appStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fffcfc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyle: {
+    color: "#6656ee",
+    fontSize: 30,
+    borderRadius: 30,
+  },
+});
+
+export default appStyle;
